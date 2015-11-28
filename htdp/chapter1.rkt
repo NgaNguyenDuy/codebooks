659213c3fdddcbbf44373f88200fa5b4995b670b
NgaNguyenDuy <kyo1508@gmail.com>
2015-11-29 06:41:34 +0800

    update code chapter 2 in htdp

--- htdp/chapter1.rkt
+++ htdp/chapter1.rkt
@@ -44,3 +44,53 @@
 
 (define (dollar->euro d)
   (* d 0.93))
+
+
+;; Exercise 2.2.3
+;; Define the program triangle. It consumes the length of a triangle's side and the perpendicular height. The program produces the area of a triangle.
+
+;; Solution:
+;; (h, b): number -> number (h for the perpendicular, and b for the lengeth of a triangle's side)
+;; Example:
+;; if the base is 10 and the height is 5, the area of triangle is 25
+;; if the base is 2 and the height is 5, the area of triangle is 5
+(define (triangle h b)
+  (* 1/2 h b))
+
+
+;; Exercise 2.2.4
+;; Define the program convert3. It consumes three digits, starting with the least significant digit, followed by the next most significant one, and so on. The program produces the corresponding number.
+;; Solution
+;; (a b c): number -> number
+;; c is hundred, b is tenth and a is unit.
+;; Example:
+;; (convert3 1 2 3) => 321
+(define (convert3 a b c)
+  (+ (* c 100) (* b 10) a))
+
+;; Exercise 2.2.5
+;;
+(define (f n)
+  "Return the result from n."
+  (+ (sqr n) 10))
+
+;; Exercise 2.3.1
+;; Utopia's tax accountants alway use programs that compute income taxes even though the tax rate is a solid, never-changing 15%. Define the program tax, which determines the tax on the gross pay.
+
+;; Also define netpay. The program determines the net pay of an employee from the number of hours worked.Assumed an hourly rate of $12.
+
+(define (tax s)
+  (* s 0.15))
+
+(define (netpay h)
+  (- (* 12 h) (tax (* 12 h))))
+
+
+;; Exercise 2.3.2
+;; Compute the bag of coin.
+;; the pennies = 1 cent, the nickels = 5 cents, the dime = 10 cents and the quaters = 25 cents.
+;;
+(define (sum-coints p n d q)
+  (+ p (* n 5) (* d 10) (* q 25)))
+
+
